5346f98af8a5f9807ee88f4e8faf8cb887dd12a7
Justin Cox <Justinkcox@ymail.com>
2023-10-27 11:04:26 -0500

    Begin styling landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,28 @@
+import Image from 'next/image'
+import styles from './landing.module.scss'
+
 export default function Home() {
    return (
       <main>
-         <h1>Main Page</h1>
+         <div className={styles['hero']}>
+            <Image
+               className={styles['hero-image']}
+               src="/landing_hero.jpg"
+               alt="landing page image"
+               priority
+               fill
+               sizes="100vw"
+               style={{
+                  objectFit: 'cover',
+               }}
+            />
+            <div className={styles['hero-content-container']}>
+               <div className={styles['hero-content-inner-container']}>
+                  <h1>Justin Cox</h1>
+                  <h2>Web Developer</h2>
+               </div>
+            </div>
+         </div>
       </main>
    )
 }
